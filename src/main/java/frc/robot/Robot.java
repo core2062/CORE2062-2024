f655646d5aa29072d8cc0e21fe97c099fc1eae20
CORE Programmers <core2062Programmers@googlegroups.com>
2024-02-03 14:38:49 -0600

    Created and tested basic logging systems.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -11,6 +11,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.util.datalog.*;
+
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -62,6 +65,17 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+    
+    //logging variables
+    StringLogEntry stringLog;
+    //logging startup
+    DataLogManager.start();
+
+    
+    //logging info
+    DataLog log = DataLogManager.getLog();
+    stringLog = new StringLogEntry(log, "logging test");
+
   }
 
   /**
